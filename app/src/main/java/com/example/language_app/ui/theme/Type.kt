89395a75309c57dcd1e90cec751ae287f9db120b
skrpld <skrpld96@gmail.com>
2025-10-21 22:52:54 +0300

    Add custom font

--- app/src/main/java/com/example/language_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/language_app/ui/theme/Type.kt
@@ -2,17 +2,37 @@ package com.example.language_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.language_app.R
+
+val Fredoka = FontFamily(
+    Font(R.font.fredoka)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Fredoka,
         fontWeight = FontWeight.Normal,
         fontSize = 140.sp,
         lineHeight = 140.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
 )
