90d444b8b4d49e67ce929863742f782c3f2404c2
skrpld <skrpld96@gmail.com>
2025-10-22 17:35:35 +0300

    update fonts

--- app/src/main/java/com/example/language_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/language_app/ui/theme/Type.kt
@@ -9,7 +9,12 @@ import androidx.compose.ui.unit.sp
 import com.example.language_app.R
 
 val Fredoka = FontFamily(
-    Font(R.font.fredoka)
+    Font(R.font.fredoka_regular),
+    Font(R.font.fredoka_bold, FontWeight.Bold),
+    Font(R.font.fredoka_semibold, FontWeight.SemiBold),
+    Font(R.font.fredoka_medium, FontWeight.Medium),
+    Font(R.font.fredoka_regular, FontWeight.Normal),
+    Font(R.font.fredoka_light, FontWeight.Light),
 )
 
 // Set of Material typography styles to start with
@@ -18,21 +23,43 @@ val Typography = Typography(
         fontFamily = Fredoka,
         fontWeight = FontWeight.Normal,
         fontSize = 140.sp,
-        lineHeight = 140.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 140.sp
     ),
     titleLarge = TextStyle(
         fontFamily = Fredoka,
-        fontWeight = FontWeight.Bold,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        lineHeight = 34.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 28.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 22.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = Fredoka,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        fontSize = 15.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Fredoka,
+        fontWeight = FontWeight.Normal,
+        fontSize = 13.sp,
+        lineHeight = 18.sp
+    ),
+
 )
